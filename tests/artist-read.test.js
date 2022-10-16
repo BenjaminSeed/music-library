824c54a55437f9e09777bdf33f2a8898f48f05f0
Benjamin <benseed.bs@gmail.com>
2022-10-16 16:28:52 +0100

    updated artist read test due to error

--- tests/artist-read.test.js
+++ tests/artist-read.test.js
@@ -4,9 +4,10 @@ const request = require('supertest');
 const getDb = require('../src/services/db');
 const app = require('../src/app');
 
-describe('update artist', () => {
+describe('read artist', () => {
   let db;
   let artists;
+
   beforeEach(async () => {
     db = await getDb();
     await Promise.all([
@@ -24,7 +25,7 @@ describe('update artist', () => {
       ]),
     ]);
 
-    [artists] = await db.query('SELECT * FROM Artist');
+    [artists] = await db.query('SELECT * from Artist');
   });
 
   afterEach(async () => {
@@ -32,29 +33,37 @@ describe('update artist', () => {
     await db.end();
   });
 
-  describe('/artist/:artistId', () => {
-    describe('PATCH', () => {
-      it('updates a single artist with the correct id', async () => {
-        const artist = artists[0];
-        const res = await request(app)
-          .patch(`/artist/${artist.id}`)
-          .send({ name: 'new name', genre: 'new genre' });
+  describe('/artist', () => {
+    describe('GET', () => {
+      it('returns all artist records in the database', async () => {
+        const res = await request(app).get('/artist').send();
 
         expect(res.status).to.equal(200);
+        expect(res.body.length).to.equal(3);
 
-        const [
-          [newArtistRecord],
-        ] = await db.query('SELECT * FROM Artist WHERE id = ?', [artist.id]);
+        res.body.forEach((artistRecord) => {
+          const expected = artists.find((a) => a.id === artistRecord.id);
 
-        expect(newArtistRecord.name).to.equal('new name');
-      });
+          expect(artistRecord).to.deep.equal(expected);
+        });
 
-      it('returns a 404 if the artist is not in the database', async () => {
-        const res = await request(app)
-          .patch('/artist/999999')
-          .send({ name: 'new name' });
-
-        expect(res.status).to.equal(404);
+        describe('/artist/:artistId', () => {
+          describe('GET', () => {
+            it('returns a single artist with the correct id', async () => {
+              const expected = artists[0];
+              const res = await request(app).get(`/artist/${expected.id}`).send();
+      
+              expect(res.status).to.equal(200);
+              expect(res.body).to.deep.equal(expected);
+            });
+      
+            it('returns a 404 if the artist is not in the database', async () => {
+              const res = await request(app).get('/artist/999999').send();
+      
+              expect(res.status).to.equal(404);
+            });
+          });
+        });
       });
     });
   });
